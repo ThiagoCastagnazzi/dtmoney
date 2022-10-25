698e765f175d7237d81955d30028bf2c7e0da338
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2022-10-25 18:53:08 -0400

    upping app

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+
+import { useState } from "react";
+
+import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
